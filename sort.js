c4767f3e389770e26ff280a54ab19ea828b080bc
James Moore <jameslmoore@gmail.com>
2017-05-31 21:09:01 -1000

    selection sort testing

--- sort.js
+++ sort.js
@@ -1,2 +1,23 @@
 
-//selection sort
+//selection sort. takes array, gens new sorted array
+
+function findSmallest(arr){
+  var smallest = arr[0];
+  var smallestIndex = 0;
+
+  for (var i = 0; i<arr.length; i++){
+    if (arr[i] < smallest){
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+  return smallestIndex;
+}
+
+function selectionSort(arr){
+  var newArray = [];
+  for (var i = 0; i < arr.length; i++){
+    var smallest = findSmallest(arr);
+    newArray.push(arr.pop(smallest));
+  }
+}
