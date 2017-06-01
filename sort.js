a390e8abaac44566e747647e9fc701c11e977517
James Moore <jameslmoore@gmail.com>
2017-05-31 21:19:31 -1000

    module exports for node

--- sort.js
+++ sort.js
@@ -1,23 +1,29 @@
 
 //selection sort. takes array, gens new sorted array
 
-function findSmallest(arr){
-  var smallest = arr[0];
-  var smallestIndex = 0;
+module.exports = {
 
-  for (var i = 0; i<arr.length; i++){
-    if (arr[i] < smallest){
-      smallest = arr[i];
-      smallestIndex = i;
+
+  fs:  function findSmallest(arr){
+      var smallest = arr[0];
+      var smallestIndex = 0;
+
+      for (var i = 0; i<arr.length; i++){
+        if (arr[i] < smallest){
+          smallest = arr[i];
+          smallestIndex = i;
+        }
+      }
+      return smallestIndex;
+    },
+
+
+  selSort:  function selectionSort(arr){
+      var newArray = [];
+      for (var i = 0; i < arr.length; i++){
+        var smallest = fs(arr);
+        newArray.push(arr.pop(smallest));
+      }
     }
-  }
-  return smallestIndex;
-}
 
-function selectionSort(arr){
-  var newArray = [];
-  for (var i = 0; i < arr.length; i++){
-    var smallest = findSmallest(arr);
-    newArray.push(arr.pop(smallest));
-  }
-}
+};
